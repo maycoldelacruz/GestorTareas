73115c773925023a3ea147c78902939c0eb1782b
Maycol De La Cruz <maycoldelacruz17@gmail.com>
2023-11-11 12:23:51 -0500

    user

--- src/main/java/com/api/GestorTareas/entity/Usuario.java
+++ src/main/java/com/api/GestorTareas/entity/Usuario.java
@@ -1,8 +1,11 @@
 package com.api.GestorTareas.entity;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.format.annotation.DateTimeFormat;
@@ -11,6 +14,8 @@ import java.util.Date;
 
 @Getter
 @Setter
+@Data
+@Entity(name = "user")
 public class Usuario {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -18,6 +23,7 @@ public class Usuario {
     private String firstName;
     private String lastName;
     @DateTimeFormat
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd" , timezone = "America/Lima")
     private Date birthdate;
     private String numberPhone;
     private String email;
